1e361a29cbaba332c95acf853d8777bd83630dcf
ck <ck@aa>
2020-10-11 23:45:21 +0300

    updating configs

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Update these four variables with values from your B2C tenant in the Azure portal
-const clientID = "93733604-cc77-4a3c-a604-87084dd55348"; // Application (client) ID of your API's application registration
-const b2cDomainHost = "fabrikamb2c.b2clogin.com";
-const tenantId = "fabrikamb2c.onmicrosoft.com"; // Alternatively, you can use your Directory (tenant) ID (a GUID)
-const policyName = "B2C_1_SUSI";
+const clientID = ""; // Application (client) ID of your API's application registration
+const b2cDomainHost = "rudolfadb2cexperiment.b2clogin.com";
+const tenantId = "rudolfadb2cexperiment.onmicrosoft.com"; // Alternatively, you can use your Directory (tenant) ID (a GUID)
+const policyName = "B2C_1_rudolfadb2cexperiment";
 
 const config = {
     identityMetadata: "https://" + b2cDomainHost + "/" + tenantId + "/" + policyName + "/v2.0/.well-known/openid-configuration/",
